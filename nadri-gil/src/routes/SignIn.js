0b2b420a473b36055b3a8c12c163da0197091af7
qor4 <qor_4@naver.com>
2022-09-03 23:26:23 +0900

    feat: 로그인 AIP 연동, 로그인 유지

--- nadri-gil/src/routes/SignIn.js
+++ nadri-gil/src/routes/SignIn.js
@@ -1,11 +1,43 @@
 import React, { useState } from 'react'
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
+import { useMutation } from "react-query";
+import { postLogin } from "../api.js"
+import { isLoginedAtom, loginIdAtom } from "../atom.js"
+import { useSetRecoilState, useRecoilValue } from "recoil";
 
 function SignIn(){
     // 아이디
     const [id, setId] = useState("")
     // 비밀번호
     const [password, setPassword] = useState("")
+    let navigate = useNavigate();
+
+    const setIsLoginedFn = useSetRecoilState(isLoginedAtom)
+    const setLoginIdFn = useSetRecoilState(loginIdAtom)
+    const islogined = useRecoilValue(isLoginedAtom)
+    console.log(islogined)
+    const { mutate, isLoading } = useMutation(postLogin, {
+        onSuccess: data => {
+          console.log(data);
+          const message = "success"
+          alert(message)
+          if(data.resultCode === 0){
+            alert(data.resultMsg)
+            // 로그인 상태 저장
+            setIsLoginedFn(true)
+            setLoginIdFn(id)
+            // 메인 페이지로 이동
+            // navigate('/')
+          }
+          else{
+            alert(data.resultMsg)
+          }
+        },
+        onError: () => {
+          alert("there was an error")
+        },
+
+      });
 
     // 아이디 입력 시
     const onIdHandler = (event) => {
@@ -19,6 +51,12 @@ function SignIn(){
 
     const onClickLogin = () =>{
         console.log(id, password)
+        mutate(
+            {
+                "loginId" : id,
+                "password" : password
+            }
+        )
     }
     return(
         <div>
