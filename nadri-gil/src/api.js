517f5a39b3ec20fc2ab3232f8d295ca71268f0e5
qor4 <qor_4@naver.com>
2022-10-07 15:44:28 +0900

    feat: 여행지 목록 페이지

--- nadri-gil/src/api.js
+++ nadri-gil/src/api.js
@@ -1,23 +1,44 @@
-const URL = "http://43.200.49.4:8080"
+const URL = "http://13.124.150.86:8080"
 
 export const postSignup = async (data) => {
-    return fetch(`${URL}/users/signUp`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json());
-  }
+  return fetch(`${URL}/users/signUp`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json());
+}
 
-  export const postLogin = async (data) => {
-    return fetch(`${URL}/users/login`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json());
-  }
+export const postLogin = async (data) => {
+  return fetch(`${URL}/users/login`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json());
+}
+
+export const getTravelsAll = async () => {
+  return fetch(`${URL}/travels/all`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json());
+}
+export const directions5api = async () => {
+  return fetch(`/api/map-direction/v1/driving?start=127.45521181369443, 36.629567359731475&goal=127.45502402553248, 36.63565971545496&option=trafast`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      "X-NCP-APIGW-API-KEY-ID":"mczr185hv8",
+      "X-NCP-APIGW-API-KEY": "fK3YTgXVtMaULn4EULEbgABK0G0lC4wrHmSUVj6o"
+    },
+  })
+    .then((response) => response.json());
+}
---
+++ nadri-gil/src/routes/TravelList.js
@@ -0,0 +1,199 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import "../index.css"
+
+
+import { getTravelsAll } from "../api.js"
+import { useQuery } from "react-query";
+
+const Container = styled.div`
+    width:60%;
+    margin:auto;
+`;
+
+const ContentList = styled.div`
+display:flex;
+flex-wrap:wrap;
+width:100%;
+margin:auto;
+margin-top:3vh;
+`
+const Content = styled.div`
+width:23.5%;
+height:25vh;
+padding:5px;
+text-align:center;
+    img{
+        width:100%;
+    }
+    a{
+        text-decoration: none;
+        color:black;
+    }
+    p{
+        font-family: 'SUIT';
+        font-size:0.8vw;
+        margin-top:1vh;
+    }
+`;
+const Select = styled.select`
+    width: 4vw;
+    height: 5vh;
+    border: none;
+    font-size:15px;
+    border-radius: 20px;
+    background-color:#c8d6ff;
+    color:#3366ff;
+    text-align-last: center;
+    text-align: center;
+`;
+
+function TravelList() {
+    const {isLoading, data} = useQuery("travelData", getTravelsAll, {
+        cacheTime: Infinity,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        onSuccess: data => {
+          // 성공시 호출
+          setTravelList(data.list)
+          console.log(data);
+        },
+        onError: e => {
+          // 실패시 호출 (401, 404 같은 error가 아니라 정말 api 호출이 실패한 경우만 호출됩니다.)
+          // 강제로 에러 발생시키려면 api단에서 throw Error 날립니다. (참조: https://react-query.tanstack.com/guides/query-functions#usage-with-fetch-and-other-clients-that-do-not-throw-by-default)
+          console.log(e.message);
+        }
+      });
+
+    // const data = [
+    //     {
+    //         "id": 1,
+    //         "name": "여행지 명1",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "여행지 명2",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 3,
+    //         "name": "여행지 명3",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 4,
+    //         "name": "여행지 명4",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 1,
+    //         "name": "여행지 명1",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "여행지 명2",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 3,
+    //         "name": "여행지 명3",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 4,
+    //         "name": "여행지 명4",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     }, {
+    //         "id": 1,
+    //         "name": "여행지 명1",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "여행지 명2",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 3,
+    //         "name": "여행지 명3",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     },
+    //     {
+    //         "id": 4,
+    //         "name": "여행지 명4",
+    //         "location": "지역(분류)",
+    //         "image": "https://user-images.githubusercontent.com/58421346/194452297-e7a076d3-5475-4cde-bd95-cbfa8342c8f6.png"
+    //     }
+    // ]
+    const OPTIONS = [
+        { value: false, name:'선택'},
+        { value: "개발", name: "개발" },
+        { value: "디자인", name: "디자인" },
+        { value: "과학", name: "과학" },
+    ];
+    const [travelList,setTravelList] = useState(data);
+    const [fieldValue, setFieldValue] =useState(false);
+
+    const SelectBox = (props) => {
+        const handleChange = (e) => {
+            setFieldValue(e.target.value);
+            let value = e.target.value 
+            if(value!=="false"){
+                let tmp = travelList.filter((e)=>e.field === value)
+                console.log(tmp)
+                setTravelList(tmp)
+            }
+            else setTravelList(data)
+        };
+        return (
+            <Select onChange={handleChange} value={fieldValue}>
+                {props.options.map((option) => (
+                    <option
+                        key={option.value}
+                        value={option.value}
+                        // defaultValue={props.defaultValue === option.value}
+                    >
+                        {option.name}
+                    </option>
+                ))}
+            </Select>  
+        );
+    };
+
+    return (
+        <Container>
+            <SelectBox options={OPTIONS} defaultValue={false}></SelectBox>
+            <ContentList>
+                
+                {isLoading?'loading...':travelList.map((e) => {
+                    return (
+                        <Content>
+                            <a href="">
+                                <img src={e.image}></img>
+                                <p>{e.name}</p>
+                            </a>
+                        </Content>
+                    )
+                })
+                }
+            </ContentList>
+        </Container>
+    )
+
+}
+
+export default TravelList;
