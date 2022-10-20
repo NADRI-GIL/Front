24f976e1dc978d864283dcdbf40cdc4835a1cb56
qor4 <qor_4@naver.com>
2022-10-21 00:19:36 +0900

    design: 로그인/회원가입 페이지 디자인 변경

--- nadri-gil/src/routes/SignIn.js
+++ nadri-gil/src/routes/SignIn.js
@@ -1,24 +1,66 @@
 import React, { useState } from 'react'
 import {Link, useNavigate} from "react-router-dom"
+import styled from "styled-components";
 import { useMutation } from "react-query";
 import { postLogin } from "../api.js"
 import { isLoginedAtom, loginIdAtom } from "../atom.js"
 import { useSetRecoilState, useRecoilValue } from "recoil";
-import cookie from 'react-cookies';
-import { useCookies } from 'react-cookie';
 
-// import { setCookie } from "Cookie";
-
-// const setCookie = (name, value, exp = 5) => {
-//   let date = new Date();
-//   // 날짜 생성
-//   date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
-//   // 저장!  setTime()은 1970년1월1일부터 경과된 시간을 밀리초로 수정함 (날짜정보를 수정)
-//   //getTime()은 1970년1월1일부터 경과된 시간을 밀리초로 표기함 (날짜정보를 가져올떄)
-//   document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
-//   //toUTCString()은 UTC 표준 시간대를 사용하여 지정된 날짜를 나타내는 문자열
-// };
-
+const Container = styled.div`
+h3{font-family: 'SUIT';}
+width:40%;
+margin:auto;
+`
+const LoginContainer = styled.div`
+width:40%;
+margin:auto;
+`
+const Hr = styled.hr`
+border:0;
+height:2px;
+background-color:#f4f4f4;
+`
+const Title = styled.p`
+font-family: 'SUIT';
+font-size:1vw;
+margin-top: 1.5vh;
+`
+const Text = styled.a`
+display:inline-block;
+font-family: 'SUIT';
+font-size:0.7vw;
+margin:2vh 0vh 0.5vh 0vh;
+`
+const Input = styled.input`
+background-color: #F6F6F6;
+font-family: 'SUIT';
+height: 4.5vh;
+width:100%;
+border-radius: 0.5vw;
+border: none;
+margin:0.5vh 0 1vh 0;
+padding-left: 1vw;
+outline-color: #3366ff;
+::placeholder{
+    font-family: 'SUIT';
+}
+&:focus {
+    background-color: white;
+}
+`
+const CompleteButton = styled.button`
+font-family: 'SUIT';
+box-sizing: border-box;
+background-color:#3366ff;
+color:#ffffff;
+border:none;
+border-radius: 10px;
+height: 6vh;
+font-size:0.8vw;
+padding:0 7vh 0 7vh;
+margin:auto;
+margin-top:5vh;
+`
 function SignIn(){
     // 아이디
     const [id, setId] = useState("")
@@ -33,10 +75,6 @@ function SignIn(){
   
     const { mutate, isLoading } = useMutation(postLogin, {
         onSuccess: data => {
-          
-          console.log(data);
-          const message = "success"
-          alert(message)
           if(data.resultCode === 0){
             alert(data.resultMsg)
             // 로그인 상태 저장
@@ -76,14 +114,22 @@ function SignIn(){
         )
     }
     return(
-        <div>
-            <h1>로그인</h1>
-            <h3>아이디</h3>
-            <input type="text" onChange={onIdHandler}/>
-            <h3>비밀번호</h3>
-            <input type="password" onChange={onPasswordHandler}/>
-            <button onClick={onClickLogin}>로그인</button>
-        </div>
+        <Container>
+            <h3>로그인</h3>
+            <Hr></Hr>
+            <LoginContainer>
+            <Title>아이디</Title>
+            <Input type="text" onChange={onIdHandler}/>
+            <Title>비밀번호</Title>
+            <Input type="password" onChange={onPasswordHandler}/>
+            <div style={{ textAlign: "center"}}>
+            <CompleteButton onClick={onClickLogin}>로그인</CompleteButton>
+            <br></br>
+            <Text href="/signUp">회원가입</Text>
+            </div>
+            
+            </LoginContainer>
+        </Container>
     )
 }
 export default SignIn;
