e047209408f44013790db9c672d81138288457b6
daeun <58421346+qor4@users.noreply.github.com>
2022-11-15 06:55:09 +0900

    Delete r-react directory

--- r-react/src/Body/view.js
+++
@@ -1,9 +0,0 @@
-import {AiFillStar, AiFillHeart } from 'react-icons/ai';
-import { BsFillBookmarkPlusFill } from 'react-icons/bs';
-
-this.state={
-    data:[],
-    date: "",
-    none_like : <AiFillStar/>,
-    like: <AiFillHeart/>
-}
