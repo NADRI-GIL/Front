e874413e931be78eacbb6c9e2e963fc150060ee1
qor4 <qor_4@naver.com>
2022-11-14 23:05:33 +0900

    feat: 푸터 추가

--- nadri-gil/src/Header.js
+++ nadri-gil/src/Header.js
@@ -4,8 +4,62 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { isLoginedAtom, loginIdAtom } from './atom';
 import "./Header.css";
 import {FiSearch} from 'react-icons/fi';
+import styled from "styled-components";
+const Footer = styled.div`
+    padding:1vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width:100%;
+    background-color: rgb(245,245,245);
+    margin-top:15vh;
+    padding-bottom:15vh;
+`;
+const Foot = styled.div`
+    width:63%;
+`;
+const FooterTop = styled.div`
+    display: flex;
+    
+`;
+const FooterMiddle = styled.div`
+    // padding-top:5vh;
+    padding-bottom:5vh;
+    border-bottom: 1px solid lightgray;
+    h5{ 
+        margin:0;
+        // padding-top:1vh;
+        margin-left:2vw;
+        font-weight: 600;
+        font-size:0.7vw;
+        color:darkgray;
+    }
+`;
+const FooterBottom = styled.div`
+    margin-top: 1vh;
+    h5{ 
+        margin:0;
+        padding-top:1vh;
+        margin-left:2vw;
+        font-weight: 600;
+        font-size:0.7vw;
+        color:darkgray;
+    }
+`;
+const FooterContest = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    h4{
+        display: inline;
+        margin-left:2vw;
+        font-weight: 600;
+        font-size:0.7vw;
+        color:darkgray;
+    }
+`;
 
-
+const FooterLogo = styled.div``;
 
 const Header = () => {
     let navigate = useNavigate();
@@ -56,6 +110,25 @@ const Header = () => {
             </header>
             <hr></hr>
             <Outlet />
+            <Footer>
+            <Foot>
+                <FooterTop>
+                    <FooterLogo>
+                        <img style={{width:"100%",height:"100%",objectFit:"fill"}} src="https://user-images.githubusercontent.com/44117975/190621512-54a2e02e-f0c3-4a6e-8243-6201ea2ee52d.PNG" alt="jjowinLogo"></img>
+                    </FooterLogo>
+                    <FooterContest><h4>여행지 추천 서비스</h4><h4>https://github.com/NADRI-GIL</h4></FooterContest>
+                </FooterTop>
+                <FooterMiddle>
+                    <h5>창업파일럿 프로젝트 01-09팀</h5>
+                    <h5>구본영 : wjqthrtk1905@naver.com</h5>
+                    <h5>김은서 : qnrqjrm03@naver.com</h5>
+                    <h5>허다은 : qor_4@naver.com</h5>
+                </FooterMiddle>
+                <FooterBottom>
+                    <h5>®주식회사메이켄 All rights reserved</h5>
+                </FooterBottom>
+            </Foot>
+        </Footer>
         </div>
     );
 };
